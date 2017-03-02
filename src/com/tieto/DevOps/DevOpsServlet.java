a66423813341aa9e6f2abbf924ec981a0f756b0a
Bariar Mrinal <mrinal.bariar@tieto.com>
2017-03-02 11:53:16 +0530

    Intial DevOps Commit

---
+++ src/com/tieto/DevOps/DevOpsServlet.java
@@ -0,0 +1,25 @@
+package com.tieto.DevOps;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DevOpsServlet
+ */
+public class DevOpsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		System.out.println("Name entered is : "+name);		
+		request.getSession().setAttribute("name", name);
+		response.sendRedirect("Final.jsp");
+	}
+
+}
